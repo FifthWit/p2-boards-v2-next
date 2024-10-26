1aa6bbed3a569f7ad03052d11194b263292fe4cc
FifthWit <fifthwitbusiness@gmail.com>
2024-10-26 10:54:08 -0500

    fixed dumb keying error

--- src/components/custom/Footer.tsx
+++ src/components/custom/Footer.tsx
@@ -1,4 +1,5 @@
 "use client"
+import React from "react"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 
@@ -31,15 +32,14 @@ export default function Footer(){
         <div className="w-full border border-t h-16 flex flex-row items-center justify-center">
             <div className="h-full w-min flex flex-row items-center justify-between">
             {links.map((link, index) => (
-                <>
-                <Button asChild key={index} className="text-muted-foreground" variant="ghost">
-                  <Link href={link.href} className="flex items-center space-x-2">
-                    <span>{link.title}</span>
-                  </Link>
-
-                </Button>
+              <React.Fragment key={index}>
+              <Button asChild className="text-muted-foreground" variant="ghost">
+                <Link href={link.href} className="flex items-center space-x-2">
+                <span>{link.title}</span>
+                </Link>
+              </Button>
               {index !== links.length - 1 ? (<span className="text-muted-foreground">|</span>) : null}
-              </>
+              </React.Fragment>
             ))}
             </div>
         </div>
