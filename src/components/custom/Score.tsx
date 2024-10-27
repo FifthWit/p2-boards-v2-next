4f63ba0720a0fcf3f9d99dc8917f4496fa190fab
FifthWit <fifthwitbusiness@gmail.com>
2024-10-27 16:34:18 -0500

    Added better Score design for mobile users with shadcn accordian

--- src/components/custom/Score.tsx
+++ src/components/custom/Score.tsx
@@ -1,8 +1,20 @@
 "use client"
-import { idToLevel, scoreToTime } from "@/lib/utils";
+import { useState, useEffect } from "react"
+import { idToLevel, rankToPoints, scoreToTime } from "@/lib/utils";
 import { MonitorPlay } from "lucide-react";
 import Link from "next/link"
 import Image from "next/image";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 
 export interface ScoreProps {
   id: number;
@@ -32,35 +44,99 @@ export interface ScoreProps {
   hideMap?: boolean
 }
 
-export default function Score({ level, userDataId, scoreData, userData, hideLink = false, hideMap = false}: ScoreProps) {
+export default function Score({ level, userDataId, scoreData, userData, hideLink = false, hideMap = false }: ScoreProps) {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const delay: number = 100;
+
+  if (isMobile) {
+    return (
+      <Accordion type="single" collapsible className="border m-2 rounded-lg">
+        <AccordionItem value="item-1">
+          <AccordionTrigger className="p-2">
+            <div className="flex flex-row items-center">
+              <Image
+              width={64}
+              height={64}
+              src={userData.avatar}
+              alt={`${userData.boardname}'s avatar`}
+              className="w-16 aspect-square rounded-full"
+              />
+              <Link className="ml-4 font-semibold truncate underline underline-offset-2" href={`/profile/${userDataId}`}>
+                {userData.boardname}
+              </Link>
+            </div>
+          </AccordionTrigger>
+          <AccordionContent className="p-2">
+            <div className="col-span-2 grid grid-cols-5 gap-2 items-center">
+              <span>{scoreData.playerRank}</span>
+              {!hideMap && (
+                hideLink ? (
+                  <span>{idToLevel(level)?.Name}</span>
+                ) : (
+                  <Link href={`/maps/${level}`}>{idToLevel(level)?.Name}</Link>
+                )
+              )}
+              <span>{scoreToTime(scoreData.score)}</span>
+              <span>Date: {new Date(scoreData.date).toLocaleDateString()}</span>
+              {scoreData.autorender_id ? (
+                <Link href={`https://autorender.p2sr.org/videos/${scoreData.autorender_id}`} className="flex items-center">
+                  <MonitorPlay />
+                </Link>
+              ) : <span />}
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    );
+  }
+
   return (
     <div className="w-full p-2 border border-muted m-1 rounded-lg grid grid-cols-3 gap-2 items-center">
       <div className="col-span-1 flex items-center">
         <Image
           width={64}
           height={64}
-          src={userData.avatar} 
-          alt={`${userData.boardname}'s avatar`} 
+          src={userData.avatar}
+          alt={`${userData.boardname}'s avatar`}
           className="w-16 aspect-square rounded-full"
         />
-        <Link className="ml-4 font-semibold truncate underline underline-offset-2" href={`/profile/${userDataId}`}>{userData.boardname}</Link>
+        <Link className="ml-4 font-semibold truncate underline underline-offset-2" href={`/profile/${userDataId}`}>
+          {userData.boardname}
+        </Link>
       </div>
       <div className="col-span-2 grid grid-cols-5 gap-2 items-center">
-            {!hideMap && (
-              hideLink ? (
-                <span>{idToLevel(level)?.Name}</span>
-              ) : (
-                <Link href={`/maps/${level}`}>{idToLevel(level)?.Name}</Link>
-              )
-            )}
-          <span>{scoreToTime(scoreData.score)}</span>
-          <span>Rank: <strong>{scoreData.playerRank}</strong></span>
-          <span>Date: {new Date(scoreData.date).toLocaleDateString()}</span>
-          {scoreData.autorender_id ? (
-            <Link href={`https://autorender.p2sr.org/videos/${scoreData.autorender_id}`} className="flex items-center">
-              <MonitorPlay />
-            </Link>
-          ) : <span />}
+        {!hideMap && (
+          hideLink ? (
+            <span>{idToLevel(level)?.Name}</span>
+          ) : (
+            <Link href={`/maps/${level}`}>{idToLevel(level)?.Name}</Link>
+          )
+        )}
+        <span>{scoreToTime(scoreData.score)}</span>
+        <HoverCard openDelay={delay} closeDelay={delay}>
+          <HoverCardTrigger>{scoreData.playerRank}</HoverCardTrigger>
+          <HoverCardContent className="w-fit h-fit p-2" align="start" side="left" alignOffset={-10}>
+            {rankToPoints(scoreData.playerRank)} Points
+          </HoverCardContent>
+        </HoverCard>
+        <span>Date: {new Date(scoreData.date).toLocaleDateString()}</span>
+        {scoreData.autorender_id ? (
+          <Link href={`https://autorender.p2sr.org/videos/${scoreData.autorender_id}`} className="flex items-center">
+            <MonitorPlay />
+          </Link>
+        ) : <span />}
       </div>
     </div>
   );
