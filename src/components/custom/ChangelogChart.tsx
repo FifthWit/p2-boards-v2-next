c44cafd347758aa3746e41562a02dc4df9103d2f
FifthWit <fifthwitbusiness@gmail.com>
2024-10-26 23:28:49 -0500

    trial 2 bruh

--- src/components/custom/ChangelogChart.tsx
+++ src/components/custom/ChangelogChart.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { TrendingUp } from "lucide-react"
 import { Bar, BarChart, CartesianGrid, XAxis } from "recharts"
 import { cn } from "@/lib/utils"
 
@@ -8,7 +7,6 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
